77786ecf95b007a8b84cca57539be7e38b320f99
Sterbenfr <sterben538@gmail.com>
2023-09-23 02:18:42 +0200

    feat(app): Creating the app in a temporary maintenance mode
    Removing stuff from create-Next-app and modification to be in a mainteance mode

--- README.md
+++ README.md
@@ -1,36 +1,30 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Sperkise Association Website
 
-## Getting Started
+Welcome to the Sperkise Association website repository! I am the developer
+responsible for managing the website of the "Club de Minéralogie et de
+Paléontologie de la Métropole Nord - Wasquehal" (Mineralogy and Paleontology
+Club of the French North Metropolis - Wasquehal).
 
-First, run the development server:
+## About Us
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+At Sperkise, we are passionate about 💎 Mineralogy 💎 and 🦖 Paleontology 🦕.
+Our mission is to facilitate communication with individuals who share our
+passion through monthly conferences (that should be open to remote participation
+via video conference soon) and an annual exposition. We also aim to keep
+everyone informed about our latest activities, which include regular outings
+organized by our club members to search for minerals and fossils almost every
+month in the northern region of France.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Explore Our Website
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+To learn more about our association, stay updated on our activities, and get in
+touch with us, please visit our website at [sperkise.com](https://sperkise.com).
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+## Reporting Issues
 
-## Learn More
+If you encounter any bugs or issues while using our website, we encourage you to
+report them. Please create an issue in this repository, and we will address it
+promptly to improve your experience.
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Thank you for your interest in Sperkise Association! We look forward to
+connecting with you and sharing our love for mineralogy and paleontology.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,24 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.container}>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>{"We'll be back soon !"}</h1>
+          <h2 className={styles.midTitle}>
+            {'This site is currently under maintenance.'}
+          </h2>
+          <h3 className={styles.subTitle}>{'Please check back soon.'}</h3>
+          <div className={styles.contact}>
+            <p>
+              {
+                'To get in touch with us, please feel free to mail us at\u00a0: '
+              }
+              <a href='mailto:sterben538@gmail.com'>{'sterben538@gmail.com'}</a>
+            </p>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
